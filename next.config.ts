534a36ce4238b208e60a4fd96f33eb405b12810b
AlanStefanov <>
2026-04-08 13:59:21 -0300

    feat: compress images on client form to max 512KB, cleanup user form debug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.112.130', 'localhost'],
+  allowedDevOrigins: ['192.168.100.9', '192.168.112.130', 'localhost'],
 };
 
 export default nextConfig;
